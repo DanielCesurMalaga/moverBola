3e0802763f1ae009fba5ad689d41d82735beb59a
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-11-04 14:02:44 +0100

    bola obedece pero estamos configurando choques no paredes

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -6,10 +6,14 @@ import java.util.ResourceBundle;
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+
+import modelo.Bola;
 
 public class Controlador implements Initializable {
     @FXML
@@ -18,35 +22,30 @@ public class Controlador implements Initializable {
     Circle bolaAzul;
 
     AnimationTimer timer;
-    int sentidoAzul;
     boolean pararAzul;
+    Bola miBola;
+    WritableImage capturaPantalla;
 
     public Controlador() {
-
-        sentidoAzul = 1;
+        
         pararAzul = false;
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                
-                    if (!pararAzul) {
-                        if ((bolaAzul.getLayoutX() + sentidoAzul + 10) >= 800) {
-                            sentidoAzul = sentidoAzul * -1;
-                        } else if ((bolaAzul.getLayoutX() + sentidoAzul - 10) <= 0) {
-                            sentidoAzul = sentidoAzul * -1;
-                        }
-                        bolaAzul.setLayoutX(bolaAzul.getLayoutX() + sentidoAzul);
-                    } else {
-                        sentidoAzul = 0;
-                    }
+                if (pararAzul) {
+                    miBola.getBola().setFill(Color.RED);
+                    timer.stop();
+                } else {
+                    moverBola();
+                }
             }
-
         };
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         panel.setFocusTraversable(true);
+        miBola = new Bola(bolaAzul);
         timer.start();
 
     }
@@ -55,13 +54,57 @@ public class Controlador implements Initializable {
         KeyCode code = tecla.getCode();
         switch (code) {
             case UP:
-                
+                miBola.setSentidoX(0);
+                miBola.setSentidoY(-1);
                 break;
-            
+            case RIGHT:
+                miBola.setSentidoX(+1);
+                miBola.setSentidoY(0);
+                break;
+            case DOWN:
+                miBola.setSentidoX(0);
+                miBola.setSentidoY(+1);
+                break;
+            case LEFT:
+                miBola.setSentidoX(-1);
+                miBola.setSentidoY(0);
+                break;
             default:
 
                 break;
         }
 
+    }
+
+    public void moverBola() {
+        if (!colision()) {
+            miBola.getBola().setLayoutX(miBola.getBola().getLayoutX() + miBola.getSentidoX());
+            miBola.getBola().setLayoutY(miBola.getBola().getLayoutY() + miBola.getSentidoY());
+        }
+    }
+
+    public boolean colision() {
+        capturaPantalla = panel.snapshot(null, null);
+        Color color = capturaPantalla.getPixelReader().getColor(
+            (int)miBola.getBola().getLayoutX() + miBola.getSentidoX() + 10, 
+            (int)miBola.getBola().getLayoutY() + miBola.getSentidoY() + 10
+            );
+        if (((miBola.getBola().getLayoutX() + miBola.getSentidoX() + 10) >= 800)
+                ||
+                ((miBola.getBola().getLayoutX() + miBola.getSentidoX() - 10) <= 0))
+                || 
+                (color == Color.WHITE)
+                )
+                {
+            pararAzul = true;
+            return true;
+        }
+        if (((miBola.getBola().getLayoutY() + miBola.getSentidoY() + 10) >= 600)
+                ||
+                ((miBola.getBola().getLayoutY() + miBola.getSentidoY() - 10) <= 0)) {
+            pararAzul = true;
+            return true;
+        }
+        return false;
     }
 }
--- src/modelo/Bola.java
+++ src/modelo/Bola.java
@@ -1,5 +1,33 @@
 package modelo;
 
+import javafx.scene.shape.Circle;
+
 public class Bola {
+    private Circle bola;
+    private int sentidoX;
+    private int sentidoY;
     
+    public Bola(Circle bola) {
+        this.bola = bola;
+        sentidoX = 0;
+        sentidoY = 0;
+    }
+    public Circle getBola() {
+        return bola;
+    }
+    public void setBola(Circle bola) {
+        this.bola = bola;
+    }
+    public int getSentidoX() {
+        return sentidoX;
+    }
+    public void setSentidoX(int sentidoX) {
+        this.sentidoX = sentidoX;
+    }
+    public int getSentidoY() {
+        return sentidoY;
+    }
+    public void setSentidoY(int sentidoY) {
+        this.sentidoY = sentidoY;
+    }
 }
