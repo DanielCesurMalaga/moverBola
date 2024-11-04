976a2db83483719a66df9be258c492ba8d44c8b3
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-11-04 12:51:11 +0100

    solo bola azul y la controlamos nosotros

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -17,25 +17,18 @@ public class Controlador implements Initializable {
     @FXML
     Circle bolaAzul;
 
-    @FXML
-    Circle bolaRoja;
-
     AnimationTimer timer;
     int sentidoAzul;
-    int sentidoRojo;
-    boolean pararTodo, pararRojo, pararAzul;
+    boolean pararAzul;
 
     public Controlador() {
 
         sentidoAzul = 1;
-        sentidoRojo = -1;
-        pararTodo = false;
         pararAzul = false;
-        pararRojo = false;
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                if (!pararTodo) {
+                
                     if (!pararAzul) {
                         if ((bolaAzul.getLayoutX() + sentidoAzul + 10) >= 800) {
                             sentidoAzul = sentidoAzul * -1;
@@ -46,20 +39,6 @@ public class Controlador implements Initializable {
                     } else {
                         sentidoAzul = 0;
                     }
-                    if (!pararRojo) {
-                        if ((bolaRoja.getLayoutX() + sentidoRojo + 10) >= 800) {
-                            sentidoRojo = sentidoRojo * -1;
-                        } else if ((bolaRoja.getLayoutX() + sentidoRojo - 10) <= 0) {
-                            sentidoRojo = sentidoRojo * -1;
-                        }
-                        bolaRoja.setLayoutX(bolaRoja.getLayoutX() + sentidoRojo);
-                    } else {
-                        sentidoRojo = 0;
-                    }
-                } else {
-                    sentidoRojo = 0;
-                    sentidoAzul = 0;
-                }
             }
 
         };
@@ -75,32 +54,10 @@ public class Controlador implements Initializable {
     public void teclaPulsada(KeyEvent tecla) {
         KeyCode code = tecla.getCode();
         switch (code) {
-            case R:
-                if (pararRojo == true) {
-                    sentidoRojo = 1;
-                    pararRojo = false;
-                } else {
-                    pararRojo = true;
-                }
+            case UP:
+                
                 break;
-            case A:
-                if (pararAzul == true) {
-                    sentidoAzul = 1;
-                    pararAzul = false;
-                } else {
-                    pararAzul = true;
-                }
-                break;
-            case T:
-                pararTodo = true;
-                break;
-            case E:
-                pararTodo = false;
-                pararAzul = false;
-                pararRojo = false;
-                sentidoAzul = 1;
-                sentidoRojo = -1;
-                break;
+            
             default:
 
                 break;
